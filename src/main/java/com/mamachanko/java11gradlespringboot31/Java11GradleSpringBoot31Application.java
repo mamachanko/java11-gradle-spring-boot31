bc7b0a86af45d9bbc74439cc00debafbd07de61a
Max Brauer <mbrauer@vmware.com>
2023-06-14 09:35:39 +0200

    initial-commit
    Signed-off-by: Max Brauer <mbrauer@vmware.com>

---
+++ src/main/java/com/mamachanko/java11gradlespringboot31/Java11GradleSpringBoot31Application.java
@@ -0,0 +1,13 @@
+package com.mamachanko.java11gradlespringboot31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java11GradleSpringBoot31Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java11GradleSpringBoot31Application.class, args);
+	}
+
+}
